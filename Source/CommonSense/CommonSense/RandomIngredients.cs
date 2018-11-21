72d32110d64fa97fd8fb92e170dcfaa85b1ec3b0
CGFighter <catgirlfighter@gmail.com>
2018-11-21 23:03:11 +0300

    #0 new feature: random ingredients; translate support; cleaned up settings

---
+++ Source/CommonSense/CommonSense/RandomIngredients.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Harmony;
+using Verse;
+using RimWorld;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.IO;
+using UnityEngine;
+
+namespace CommonSense
+{
+    class RandomIngredients
+    {
+        //public static Thing MakeThing(ThingDef def, ThingDef stuff = null)
+        [HarmonyPatch(typeof(ThingMaker), "MakeThing", new Type[] { typeof(ThingDef), typeof(ThingDef) })]
+        static class ThingMaker_MakeThing_CommonSensePatch
+        {
+            public static Dictionary<ThingDef, RecipeDef> hTable = new Dictionary<ThingDef, RecipeDef>();
+            static void Postfix(Thing __result, ThingDef def, ThingDef stuff)
+            {
+                if (!Settings.add_meal_ingredients || __result == null || stuff != null)
+                    return;
+
+                CompIngredients ings = __result.TryGetComp<CompIngredients>();
+                if (ings == null || ings.ingredients.Count > 0)
+                    return;
+
+                RecipeDef d = hTable.TryGetValue(def);
+                if (d == null)
+                {
+                    d = DefDatabase<RecipeDef>.AllDefsListForReading.Where(x => x.products.Any(y => y.thingDef == def)).RandomElement();
+                    if (d == null)
+                        return;
+                    hTable.Add(def, d);
+                }
+
+                foreach (IngredientCount c in d.ingredients)
+                {
+                    ThingDef td = c.filter.AllowedThingDefs.Where(
+                        x => !x.IsIngestible || x.thingClass != typeof(ThingWithComps) && !FoodUtility.IsHumanlikeMeat(x) && x.ingestible.specialThoughtAsIngredient == null
+                    ).RandomElement();
+                    if (td != null)
+                        ings.RegisterIngredient(td);
+                }
+            }
+        }
+
+        //public static IEnumerable<Thing> MakeRecipeProducts(RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Thing dominantIngredient, IBillGiver billGiver)
+        [HarmonyPatch]
+        static class GenRecipe_MakeRecipeProducts_CommonSensePatch
+        {
+            private static FieldInfo ingredientsCompField;
+
+            internal static void ClearIngs(CompIngredients ings)
+            {
+                ings.ingredients.Clear();
+            }
+
+            internal static MethodBase TargetMethod()
+            {
+                Type nestedTypeResult = null;
+                const string targetMethod = "<MakeRecipeProducts>";
+                foreach (var nestedType in typeof(GenRecipe).GetNestedTypes(AccessTools.all))
+                {
+                    if (!nestedType.Name.Contains(targetMethod)) continue;
+                    nestedTypeResult = nestedType;
+                }
+
+                if (nestedTypeResult == null) throw new Exception($"Could not find {targetMethod} Iterator Class");
+
+                if (ingredientsCompField == null)
+                {
+                    var fields = AccessTools.GetDeclaredFields(nestedTypeResult);
+                    foreach (var i in fields)
+                    {
+                        if (i.FieldType == typeof(CompIngredients))
+                            if (ingredientsCompField == null)
+                                ingredientsCompField = i;
+                            else
+                                throw new Exception("Multiple CompIngredients fields found");
+                    }
+                }
+
+                if (ingredientsCompField == null) throw new Exception($"Could not find (CompIngredients) in {nestedTypeResult.FullName}.DeclaredFields");
+
+                var result = AccessTools.Method(nestedTypeResult, "MoveNext");
+
+                if (result == null) throw new Exception($"Could not find MoveNext in {nestedTypeResult.FullName}");
+
+                return result;
+            }
+
+            [HarmonyTranspiler]
+            internal static IEnumerable<CodeInstruction> CleanIngList(IEnumerable<CodeInstruction> instrs)
+            {
+
+                CodeInstruction prei = null;
+
+                foreach (var i in (instrs))
+                {
+                    yield return i;
+
+                    if (i.opcode == OpCodes.Brfalse && prei.opcode == OpCodes.Ldfld && prei.operand == ingredientsCompField)
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        yield return new CodeInstruction(OpCodes.Ldfld, ingredientsCompField);
+                        yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(GenRecipe_MakeRecipeProducts_CommonSensePatch), nameof(GenRecipe_TryDispenseFood_CommonSensePatch.ClearIngs), new Type[] { typeof(CompIngredients) }));
+                    }
+                    prei = i;
+                }
+            }
+        }
+
+        //public virtual Thing TryDispenseFood()
+        [HarmonyPatch(typeof(Building_NutrientPasteDispenser), "TryDispenseFood")]
+        static class GenRecipe_TryDispenseFood_CommonSensePatch
+        {
+            public static void ClearIngs(CompIngredients ings)
+            {
+                ings.ingredients.Clear();
+            }
+
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> CleanIngList(IEnumerable<CodeInstruction> instrs)
+            {
+                foreach (CodeInstruction instr in instrs)
+                {
+                    yield return instr;
+                    if (instr.opcode == OpCodes.Stloc_S && instr.operand is LocalBuilder && ((LocalBuilder)instr.operand).LocalType == typeof(CompIngredients))
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldloc_S, instr.operand);
+                        yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(GenRecipe_MakeRecipeProducts_CommonSensePatch), nameof(GenRecipe_TryDispenseFood_CommonSensePatch.ClearIngs), new Type[] { typeof(CompIngredients) }));
+                    }
+                }
+            }
+        }
+    }   
+}
--- Source/CommonSense/CommonSense/Settings.cs
+++ Source/CommonSense/CommonSense/Settings.cs
@@ -10,18 +10,28 @@ namespace CommonSense
         public static bool fulfill_outdoors = true;
         public static bool odd_is_normal = false;
         public static bool clean_before_work = true;
+        public static bool clean_after_tanding = true;
         public static bool calculate_full_path = true;
+        public static bool add_meal_ingredients = false;
 
         public static void DoSettingsWindowContents(Rect inRect)
         {
             Listing_Standard listing_Standard = new Listing_Standard();
             listing_Standard.Begin(inRect);
-            listing_Standard.CheckboxLabeled("Pawns are encouraged to fulfill their need of outdoors by seeking recreation outdoors", ref fulfill_outdoors, "NOTE: Don't place the same joy giving sources both indoors and outdoors to avoid indecisiveness.");
-            listing_Standard.CheckboxLabeled("Pawns are encouraged to clean the room before aperating a building in it", ref clean_before_work,"NOTE: don't exptect cleaning from those guys, who doesn't like to clean");
-            listing_Standard.CheckboxLabeled("Calculate accurate path to the building for bill jobs", ref calculate_full_path, "It'll help to avoid making unreasonable hooks around when your pawns need to bring resources first, but it may be costly on CPU");
-            listing_Standard.CheckboxLabeled("Separate meals with negative thoughts about them", ref separate_meals);
-            listing_Standard.CheckboxLabeled("Count odd meat (ex. insect) as normal, allowing to stack it with normal meals", ref odd_is_normal);
-
+            listing_Standard.Label("fulfill_outdoors_head".Translate());
+            listing_Standard.CheckboxLabeled("fulfill_outdoors_label".Translate(), ref fulfill_outdoors, "fulfill_outdoors_note".Translate());
+            listing_Standard.GapLine();
+            listing_Standard.Label("clean_head".Translate());
+            listing_Standard.CheckboxLabeled("clean_before_working_label".Translate(), ref clean_before_work, "clean_before_working_note".Translate());
+            listing_Standard.CheckboxLabeled("bill_job_pathing_label".Translate(), ref calculate_full_path, "bill_job_pathing_note".Translate());
+            listing_Standard.CheckboxLabeled("clean_after_tending_label".Translate(), ref clean_after_tanding, "clean_after_tending_note".Translate());
+            listing_Standard.GapLine();
+            listing_Standard.Label("meal_stacking_head".Translate());
+            listing_Standard.CheckboxLabeled("meal_stacking_label".Translate(), ref separate_meals, "meal_stacking_note".Translate());
+            listing_Standard.CheckboxLabeled("dont_count_odd_label".Translate(), ref odd_is_normal, "dont_count_odd_note".Translate());
+            listing_Standard.GapLine();
+            listing_Standard.Label("gen_ingredients_head".Translate());
+            listing_Standard.CheckboxLabeled("gen_ingredients_label".Translate(), ref add_meal_ingredients, "gen_ingredients_note".Translate());
             listing_Standard.End();
         }
 
@@ -32,7 +42,9 @@ namespace CommonSense
             Scribe_Values.Look<bool>(ref fulfill_outdoors, "fulfill_outdoors", true, false);
             Scribe_Values.Look<bool>(ref odd_is_normal, "odd_is_normal", false, false);
             Scribe_Values.Look<bool>(ref clean_before_work, "clean_before_work", true, false);
+            Scribe_Values.Look<bool>(ref clean_after_tanding, "clean_after_tanding", true, false);
             Scribe_Values.Look<bool>(ref calculate_full_path, "calculate_full_path", true, false);
+            Scribe_Values.Look<bool>(ref add_meal_ingredients, "add_meal_ingredients", false, false);
         }
     }
 }
