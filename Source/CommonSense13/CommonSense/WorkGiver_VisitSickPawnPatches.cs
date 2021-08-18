1beb831fb631b9790479a2a1c4f54a5b0fd4d06d
CGFighter <catgirlfighter@gmail.com>
2021-08-18 16:50:02 +0300

    1.1.43

--- Source/CommonSense13/CommonSense/WorkGiver_VisitSickPawnPatches.cs
+++ Source/CommonSense13/CommonSense/WorkGiver_VisitSickPawnPatches.cs
@@ -4,9 +4,61 @@ using RimWorld;
 using Verse;
 using Verse.AI;
 using UnityEngine;
+using System;
 
 namespace CommonSense
 {
+    public class JoyGiver_TakeDrug_Patient : JoyGiver_Ingest
+    {
+        private static List<ThingDef> takeableDrugs = new List<ThingDef>();
+
+        protected override Thing BestIngestItem(Pawn pawn, Predicate<Thing> extraValidator)
+        {
+            if (pawn.drugs == null)
+            {
+                return null;
+            }
+            Predicate<Thing> predicate = (Thing t) => CanIngestForJoy(pawn, t) && (extraValidator == null || extraValidator(t)) && t.def.ingestible != null && t.def.ingestible.drugCategory != DrugCategory.None;
+            ThingOwner<Thing> innerContainer = pawn.inventory.innerContainer;
+            for (int i = 0; i < innerContainer.Count; i++)
+            {
+                if (predicate(innerContainer[i]))
+                {
+                    return innerContainer[i];
+                }
+            }
+            //bool flag = false;
+            //if (pawn.story != null && (pawn.story.traits.DegreeOfTrait(TraitDefOf.DrugDesire) > 0 || pawn.InMentalState))
+            //{
+            //    flag = true;
+            //}
+            takeableDrugs.Clear();
+            DrugPolicy currentPolicy = pawn.drugs.CurrentPolicy;
+            for (int j = 0; j < currentPolicy.Count; j++)
+            {
+                if (/*flag ||*/ currentPolicy[j].allowedForJoy)
+                {
+                    takeableDrugs.Add(currentPolicy[j].drug);
+                }
+            }
+            //
+            takeableDrugs.Shuffle();
+            for (int k = 0; k < takeableDrugs.Count; k++)
+            {
+                List<Thing> list = pawn.Map.listerThings.ThingsOfDef(takeableDrugs[k]);
+                if (list.Count > 0)
+                {
+                    Thing thing = GenClosest.ClosestThing_Global_Reachable(pawn.Position, pawn.Map, list, PathEndMode.OnCell, TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false, false, false), 9999f, predicate, null);
+                    if (thing != null)
+                    {
+                        return thing;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
     [HarmonyPatch(typeof(WorkGiver_VisitSickPawn), "JobOnThing")]
     static class WorkGiver_VisitSickPawn_JobOnThing_CommonSensePatch
     {
@@ -18,14 +70,14 @@ namespace CommonSense
             Pawn sick = (Pawn)t;
             List<JoyGiverDef> defs = DefDatabase<JoyGiverDef>.AllDefsListForReading.FindAll(
                 d => d.giverClass == typeof(JoyGiver_Ingest) 
-                || d.giverClass == typeof(JoyGiver_TakeDrug)
+                || d.giverClass == typeof(JoyGiver_TakeDrug_Patient)
                 || d.giverClass == typeof(JoyGiver_VisitSickPawn));
             //
             var chances = new DefMap<JoyGiverDef, float>();
             foreach (var d in defs)
             {
-                if (d.Worker.CanBeGivenTo(sick))
-                    chances[d] = d.Worker.GetChance(sick) * Mathf.Max(0.001f, Mathf.Pow(1f - sick.needs.joy.tolerances[d.joyKind], 5f));
+                if (d.Worker.CanBeGivenTo(pawn))
+                    chances[d] = (d.giverClass == typeof(JoyGiver_TakeDrug_Patient) ? 4f : d.Worker.GetChance(sick)) * Mathf.Max(0.001f, Mathf.Pow(1f - sick.needs.joy.tolerances[d.joyKind], 5f));
                 else
                     chances[d] = 0f;
             }
